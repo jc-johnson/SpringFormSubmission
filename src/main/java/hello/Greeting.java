950dfc3c3b6d2c5de806f1892005f73ef5aa480a
Jordan <jjohnson@gorillagroup.com>
2016-06-23 14:38:48 -0500

    working on bug

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,26 @@
+package hello;
+
+/**
+ * Created by jjohnson on 6/23/2016.
+ */
+
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
